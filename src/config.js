18f5e2dac8c91b8762c1a4401d9eb05e81f084ac
Muhammadaziz <84275085+Muhammadaziz107@users.noreply.github.com>
2022-06-07 11:38:30 +0500

    add

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const PORT = process.env.PORT || 4000;
 const PG = {
-  connectionElString:
-    "postgres://tjcqfswt:jQkgm2JdFIPmyas3mp9_AnVnSdUsK5Dz@kashin.db.elephantsql.com/tjcqfswt",
+  connectionElString: "http://localhost:4000/61c72826297f2a5e29874e3d",
+  connectionElString: "http://localhost:4000/61c72826297f2a5e29874e3d",
 };
 
 module.exports = {
